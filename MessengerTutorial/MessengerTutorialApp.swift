53cf8ed8ca5ad87b15c05e58e72e4991430dd3bd
lmsand <lmsand99@icloud.com>
2023-12-14 20:25:59 -0600

    Initial Commit

---
+++ MessengerTutorial/MessengerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialApp.swift
+//  MessengerTutorial
+//
+//  Created by lexi sanders on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
