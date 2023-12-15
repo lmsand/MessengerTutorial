127a14b4f697bf259b908686990949a280882346
lmsand <lmsand99@icloud.com>
2023-12-14 21:06:46 -0600

    Sign Up View UI

---
+++ MessengerTutorial/Core/Authentication/View/RegistrationView.swift
@@ -0,0 +1,88 @@
+//
+//  RegistrationView.swift
+//  MessengerTutorial
+//
+//  Created by lexi sanders on 12/14/23.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var fullname = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            
+            Spacer()
+            
+            // logo image
+            Image("messenger-icon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                .padding()
+            
+            // text fields
+            VStack {
+                TextField("Enter your email", text: $email)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+                
+                TextField("Enter your fullname", text: $fullname)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+                
+                SecureField("Enter your password", text: $password)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+            }
+            
+            Button {
+                print("Handle sign up")
+            } label: {
+                Text("Sign Up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 360, height: 44)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(10)
+            }
+            .padding(.vertical)
+            
+            Spacer()
+            
+            Divider()
+            
+            Button {
+                dismiss()
+            } label: {
+                HStack(spacing: 3) {
+                    Text("Already have an account?")
+                    
+                    Text("Sign In")
+                        .fontWeight(.semibold)
+                }
+                .font(.footnote)
+            }
+            .padding(.vertical)
+        }
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
