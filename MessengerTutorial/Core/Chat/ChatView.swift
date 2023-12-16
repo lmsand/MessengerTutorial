feb5031d0f53c72025944537ab2d444aba760a7a
lmsand <lmsand99@icloud.com>
2023-12-16 12:51:15 -0600

    Chat UI

---
+++ MessengerTutorial/Core/Chat/ChatView.swift
@@ -0,0 +1,61 @@
+//
+//  ChatView.swift
+//  MessengerTutorial
+//
+//  Created by lexi sanders on 12/16/23.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    @State private var messageText = ""
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                // header
+                VStack {
+                    CircularProfileImageView(user: User.MOCK_USER, size: .xLarge)
+                    
+                    VStack(spacing: 4) {
+                        Text("Bruce Wayne")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        Text("Messenger")
+                            .font(.footnote)
+                            .foregroundStyle(.gray)
+                    }
+                }
+                
+                // messages
+            }
+            
+            // message input view
+            
+            Spacer()
+            
+            ZStack(alignment: .trailing) {
+                TextField("Message...", text: $messageText, axis: .vertical)
+                    .padding(12)
+                    .padding(.trailing, 48)
+                    .background(Color(.systemGroupedBackground))
+                    .clipShape(Capsule())
+                    .font(.subheadline)
+                
+                Button {
+                    print("Send message")
+                } label: {
+                    Text("Send")
+                        .fontWeight(.semibold)
+                }
+                .padding(.horizontal)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ChatView()
+}
